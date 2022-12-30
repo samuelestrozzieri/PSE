1ffea7907c8b18df06a27de10d5d643be45f254c
Samuele Strozzieri <strozzierisamuele.ss@libero.it>
2022-12-30 18:04:49 +0100

    Modified Obstacle constructor
    added if statments to controll valid data members

--- Assignment/Obstacle.cpp
+++ Assignment/Obstacle.cpp
@@ -11,6 +11,14 @@
 Obstacle::Obstacle(double x1, double y1, double x2, double y2)
          :x1_{x1}, y1_{y1}, x2_{x2}, y2_{y2}
 {
+    if (x1>x2){
+        std::cerr << "Obstacle::Obstacle(double x1,...): x1 must be less than x2\n";
+        exit(EXIT_FAILURE);
+    }
+    if (y1>y2){
+        std::cerr << "Obstacle::Obstacle(double x1,...): y1 must be less than y2\n";
+        exit(EXIT_FAILURE);
+    }
     if ((remainder(abs(x1-x2),1.0) > .1e-17)||(remainder((y1-y2),1.0) > .1e-17)){
         std::cerr << "Obstacle::Obstacle(double x1,...): invalid obstacle position(s) inserted\n";
         exit(EXIT_FAILURE);
