6cc655b1747b48345bad06ef42fc4ed12eff1493
samuelestrozzieri <116587597+samuelestrozzieri@users.noreply.github.com>
2023-01-03 14:42:31 +0100

    modified: Assignment/helperfunc.cpp added nearest_obs(array<double,2> Robot, vector<Obstacle>& vecObs) function and ordered functions in helperfunc.cpp

--- Assignment/helperfunc.cpp
+++ Assignment/helperfunc.cpp
@@ -13,6 +13,18 @@ using std::array;
 #include "Robot.h"
 #include "Obstacle.h"
 
+//Buona per il potenziale attrattivo a = robot b = goal
+//Compute Euclidean distance between two points inserting their coordinates x and y
+double eudis(double Xa, double Ya, double Xb, double Yb)
+{
+   return hypot ((Xa-Xb), (Ya-Yb));
+}
+//Compute Euclidean distance between two points inserting them as array of double_type coordinates {x,y}
+double eudis(std::array<double,2> pointA, std::array<double,2> pointB)
+{
+   return hypot ((pointA[0]-pointB[0]), (pointA[1]-pointB[1]));
+}
+
 std::pair<Obstacle,double> nearest_obs(double Xpos, double Ypos, vector<Obstacle> vecObs){
     double dist;
     array<double,2> Edgem;
@@ -23,9 +35,9 @@ std::pair<Obstacle,double> nearest_obs(double Xpos, double Ypos, vector<Obstacle
         Edgem = (*pos).get_edgeMin();
         EdgeM = (*pos).get_edgeMax();
         if (Xpos <= EdgeM[0] && Xpos >= Edgem[0]) 
-            dist = std::min(std::abs(Xpos - EdgeM[0]), std::abs(Xpos - Edgem[0]));
-        else if (Ypos <= EdgeM[1] && Ypos >= Edgem[1])
             dist = std::min(std::abs(Ypos - EdgeM[1]), std::abs(Ypos - Edgem[1]));
+        else if (Ypos <= EdgeM[1] && Ypos >= Edgem[1])
+            dist = std::min(std::abs(Xpos - EdgeM[0]), std::abs(Xpos - Edgem[0]));
         else if (Xpos < Edgem[0])
         {
             if (Ypos > EdgeM[1]) 
@@ -54,21 +66,50 @@ std::pair<Obstacle,double> nearest_obs(double Xpos, double Ypos, vector<Obstacle
         }
     }
 }
+
 std::pair<Obstacle,double> nearest_obs(array<double,2> Robot, vector<Obstacle> vecObs){
-    
+    double dist = -1.0;
+    array<double,2> Edgem;
+    array<double,2> EdgeM;
+    pair<Obstacle,double> p;
+    for (vector<Obstacle>::const_iterator pos = vecObs.cbegin(); pos != vecObs.cend(); ++pos)
+    {
+        Edgem = (*pos).get_edgeMin();
+        EdgeM = (*pos).get_edgeMax();
+        if (Robot[0] <= EdgeM[0] && Robot[0] >= Edgem[0]) 
+            dist = std::min(std::abs(Robot[1] - EdgeM[1]), std::abs(Robot[1] - Edgem[1]));
+        else if (Robot[1] <= EdgeM[1] && Robot[1] >= Edgem[1])
+            dist = std::min(std::abs(Robot[0] - EdgeM[0]), std::abs(Robot[0] - Edgem[0]));
+        else if (Robot[0] < Edgem[0])
+        {
+            if (Robot[1] > EdgeM[1]) 
+                dist = eudis(Robot[0],Robot[1],Edgem[0],EdgeM[1]);
+            if(Robot[1] < Edgem[1])
+                dist = eudis(Robot,Edgem);
+        }
+        else if (Robot[0] > EdgeM[0])
+        {
+            if (Robot[1] > EdgeM[1]) 
+                dist = eudis(Robot,EdgeM);
+            else if(Robot[1] < Edgem[1])
+                dist = eudis(Robot[0],Robot[1],EdgeM[0],Edgem[1]);
+        }
+        else
+        {
+            std::cerr << "nearest_obs(): something gone wrong\n";
+            exit(EXIT_FAILURE);
+        }
+        if (pos == vecObs.cbegin())
+             p = std::make_pair(*pos,dist);
+        if((pos != vecObs.cbegin())&&(dist < p.second))
+        {
+            p.first = *pos;
+            p.second = dist;
+        }
+    }
+    return p;
 }
 
-//Buona per il potenziale attrattivo a = robot b = goal
-//Compute Euclidean distance between two points inserting their coordinates x and y
-double eudis(double Xa, double Ya, double Xb, double Yb)
-{
-   return hypot ((Xa-Xb), (Ya-Yb));
-}
-//Compute Euclidean distance between two points inserting them as array of double_type coordinates {x,y}
-double eudis(std::array<double,2> pointA, std::array<double,2> pointB)
-{
-   return hypot ((pointA[0]-pointB[0]), (pointA[1]-pointB[1]));
-}
 
 int main(){
 
